f0fe9fd9e393fc1e4211637c5576ce5cedb211a5
MAPIR <math.perlier@gmail.com>
2021-11-11 17:12:52 +0100

    first commit

---
+++ public/php/connect.php
@@ -0,0 +1,32 @@
+<?php
+class Connect 
+{
+  public static $dbhost = 'localhost';
+  public static $dbname = 'qr';
+  public static $user = 'root';
+  public static $pass = 'root';
+
+  private static $connection = null;
+
+  public static function connect()
+  {
+    if(self::$connection == null)
+    {
+      try
+      {
+        self::$connection = new PDO('mysql:host='.self::$dbhost.';dbname='.self::$dbname.';charset=utf8', self::$user, self::$pass);
+      }
+      catch(PDOException $e)
+      {
+          die($e->getMessage());
+      }
+    }
+    return self::$connection;
+  }
+
+  public static function disconnect()
+  {
+      self::$connection = null;
+  }
+}
+?>
